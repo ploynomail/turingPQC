34b0b1242d822fe262d8565290b83f0b0e9c6f79
hongmin.zhang <cyrus_zhang@163.com>
2024-11-14 10:30:52 +0000

    change example

--- example/sm2dilithium2hybrid/main.go
+++ example/sm2dilithium2hybrid/main.go
@@ -26,7 +26,8 @@ func main() {
 		BasicConstraintsValid: true,
 		IsCA:                  true,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		// Digital Signature, Non Repudiation, Key Encipherment, Data Encipherment, Key Agreement, Certificate Sign, CRL Sign
+		KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign | x509.KeyUsageKeyEncipherment | x509.KeyUsageDataEncipherment | x509.KeyUsageKeyAgreement | x509.KeyUsageContentCommitment,
 	}
 	// 私钥及公钥 格式
 	caSelfSignedPrivateKey, _ := sm2dilithium2hybrid.GenerateKey(rand.Reader)
@@ -53,4 +54,63 @@ func main() {
 	pem.Encode(os.Stdout, &pem.Block{Type: "CERTIFICATE", Bytes: caSelfSigned})
 	log.Println("write to", caSelfSignedPrivateKeyFile)
 	os.WriteFile(caSelfSignedPrivateKeyFile, caSelfSignedPrivateKeyDER, 0777) // 将 DER 编码私钥写入文件
+
+	// 待签署证书及其私钥公钥==================================
+	sigeCert("lalala", ca, caSelfSignedPrivateKey, caSelfSigned)
+	sigeCert("lalala2", ca, caSelfSignedPrivateKey, caSelfSigned)
+}
+
+func sigeCert(cn string, ca *x509.Certificate, caSelfSignedPrivateKey *sm2dilithium2hybrid.PrivateKey, caSelfSigned []byte) error {
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(1658),
+		Subject: pkix.Name{
+			Country: []string{cn},
+		},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature | x509.KeyUsageContentCommitment,
+	}
+	certPrivateKey, _ := sm2dilithium2hybrid.GenerateKey(rand.Reader)
+	certPublicKey := &certPrivateKey.PublicKey
+
+	// 使用自签CA 对 证书签署
+	certSigned, err2 := x509.CreateCertificate(rand.Reader, cert, ca, certPublicKey, caSelfSignedPrivateKey)
+	if err2 != nil {
+		log.Println("create cert2 failed", err2)
+		return err2
+	}
+
+	certFile := cn + ".pem"
+	log.Println("write to", certFile)
+	os.WriteFile(certFile, certSigned, 0777) // cert 写入文件
+
+	certPrivateKeyFile := cn + ".key"
+	certPrivateKeyDER, err := x509.MarshalPKCS8PrivateKey(certPrivateKey) // 将私钥转换为 DER 编码格式
+	if err != nil {
+		log.Println("marshal pkcs8 failed", err)
+		return err
+	}
+	log.Println("write to", certPrivateKeyFile)
+	os.WriteFile(certPrivateKeyFile, certPrivateKeyDER, 0777) // 私钥写入文件
+	pem.Encode(os.Stdout, &pem.Block{Type: "PRIVATE KEY", Bytes: certPrivateKeyDER})
+	pem.Encode(os.Stdout, &pem.Block{Type: "CERTIFICATE", Bytes: certSigned})
+	ca_tr, err := x509.ParseCertificate(caSelfSigned)
+
+	if err != nil {
+		log.Println("parse ca failed", err)
+		return err
+	}
+
+	cert_tr, err := x509.ParseCertificate(certSigned)
+
+	if err != nil {
+		log.Println("parse cert failed", err)
+		return err
+	}
+
+	err = cert_tr.CheckSignatureFrom(ca_tr)
+	log.Println("check signature", err)
+	return nil
 }
